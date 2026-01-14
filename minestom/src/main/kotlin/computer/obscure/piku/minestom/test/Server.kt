347c18183c6cc7dc6db324120327ebf7086c40ac
zNotChill <znotchill@znci.dev>
2026-01-14 00:41:05 +0000

    testserver: accurate loading screen

--- minestom/src/main/kotlin/computer/obscure/piku/minestom/test/Server.kt
+++ minestom/src/main/kotlin/computer/obscure/piku/minestom/test/Server.kt
@@ -4,11 +4,15 @@ import computer.obscure.piku.common.scripting.api.LuaEventData
 import me.znotchill.blossom.extensions.addListener
 import me.znotchill.blossom.server.BlossomServer
 import computer.obscure.piku.minestom.scripting.MinestomAPI
-import me.znotchill.blossom.command.command
+import me.znotchill.blossom.extensions.ticks
+import me.znotchill.blossom.scheduler.task
 import net.minestom.server.entity.GameMode
 import net.minestom.server.event.player.AsyncPlayerConfigurationEvent
+import net.minestom.server.event.player.PlayerChunkLoadEvent
+import net.minestom.server.event.player.PlayerChunkUnloadEvent
 import net.minestom.server.event.player.PlayerLoadedEvent
 import net.minestom.server.instance.InstanceContainer
+import java.util.UUID
 
 class Server : BlossomServer(
     auth = false
@@ -16,6 +20,11 @@ class Server : BlossomServer(
     lateinit var instanceContainer: InstanceContainer
 
     val piku = MinestomAPI(this)
+
+    val loadedChunks = mutableMapOf<UUID, MutableSet<Long>>()
+
+    fun pack(x: Int, z: Int): Long =
+        (x.toLong() shl 32) or (z.toLong() and 0xffffffff)
 
     override fun preLoad() {
         piku.registerEvents()
@@ -34,23 +43,69 @@ class Server : BlossomServer(
                 resourceDirectory = "scripts/client",
                 recurse = true
             )
-        }
 
-        registerCommand(
-            command("test") {
-                syntax {
+            scheduler.task {
+                delay = 1.ticks
+                run = {
                     piku.sendData(
-                        this,
+                        event.player,
                         "load_map",
                         LuaEventData(
-                            mapOf(
-                                "chunk_count" to 500
-                            )
+                            emptyMap()
                         )
                     )
                 }
             }
-        )
+        }
+
+        eventHandler.addListener<PlayerChunkUnloadEvent> { event ->
+            val set = loadedChunks[event.player.uuid] ?: return@addListener
+            set.remove(pack(event.chunkX, event.chunkZ))
+        }
+
+        eventHandler.addListener<PlayerChunkLoadEvent> { event ->
+            val player = event.player
+            val uuid = player.uuid
+
+            val set = loadedChunks.getOrPut(uuid) { mutableSetOf() }
+            set.add(pack(event.chunkX, event.chunkZ))
+
+            val loaded = set.size
+
+            val px = player.chunk?.chunkX ?: 0
+            val pz = player.chunk?.chunkX ?: 0
+
+            val radius = set.maxOf {
+                val x = (it shr 32).toInt()
+                val z = it.toInt()
+                maxOf(kotlin.math.abs(x - px), kotlin.math.abs(z - pz))
+            }
+
+            val total = (radius * 2 + 1) * (radius * 2 + 1)
+
+            piku.sendData(
+                player,
+                "load_map_progress",
+                LuaEventData(
+                    mapOf(
+                        "loaded" to loaded,
+                        "total" to total
+                    )
+                )
+            )
+
+            if (total == loaded) {
+                piku.sendData(
+                    player,
+                    "load_map_done",
+                    LuaEventData(
+                        emptyMap()
+                    )
+                )
+            }
+        }
+
+
 
         piku.runAllScripts(
             resourceDirectory = "scripts/server",
--- minestom/src/main/resources/scripts/client/ui/loading_screen.lua
+++ minestom/src/main/resources/scripts/client/ui/loading_screen.lua
@@ -1,16 +1,69 @@
 
+local ui = game.ui
+local mapLoading = false
+
 listen("load_map", function(event)
-    local chunkCount = tonumber(event["chunk_count"])
-    render(chunkCount)
+    print("hi")
+    render()
+    mapLoading = true
 end)
 
-local ui = game.ui
+listen("load_map_progress", function(event)
+    if not mapLoading then return end
 
-function render(chunkCount)
+    local loaded = tonumber(event["loaded"])
+    local total = tonumber(event["total"])
+
+    ui.get("loading_screen")
+        .get("asset_counter")
+        .text(loaded .. "/" .. total)
+
+    ui.get("loading_screen").get("test_bar").animate()
+        .progress(loaded / total, 0.25, "EASE_IN_OUT_CUBIC")
+        .play()
+end)
+
+listen("load_map_done", function(event)
+    print("done loading chunks")
+
+    local screen = ui.get("loading_screen")
+
+    local fadeOutTime = 3
+    local easing = "EASE_OUT_BACK"
+    screen.get("background").animate()
+        .opacity(0, fadeOutTime, easing)
+        .play()
+    screen.get("test_sprite").animate()
+        .opacity(0, fadeOutTime, easing)
+        .move(vec2.of(0, -50), fadeOutTime, easing)
+        .play()
+    screen.get("test_text").animate()
+        .scale(vec2.of(3, 3), fadeOutTime, easing)
+        .opacity(0, fadeOutTime, easing)
+        .play()
+    screen.get("test_bar").animate()
+        .opacity(0, fadeOutTime, easing)
+        .move(vec2.of(0, 5), fadeOutTime, easing)
+        .play()
+    screen.get("asset_counter").animate()
+        .opacity(0, fadeOutTime, easing)
+        .move(vec2.of(0, 10), fadeOutTime, easing)
+        .play()
+
+    scheduler
+        .task(function(task)
+            ui.get("loading_screen").remove()
+            screenUnloaded = true
+        end)
+        .delay(40)
+        .run()
+end)
+
+function render()
     local group = ui.group("loading_screen")
 
     local loadedChunkCount = 0
-    local screenUnloaded = false
+    local chunkCount = 60000
 
     local box = group.box("background")
         .color(color.rgb(0, 0, 0))
@@ -49,23 +102,6 @@ function render(chunkCount)
         .pos(vec2.of(0, 10))
         .opacity(0)
 
-    scheduler
-        .task(function(task)
-            if screenUnloaded then
-                task.cancel()
-                return
-            end
-            if loadedChunkCount >= chunkCount then
-                task.cancel()
-                return
-            end
-            loadedChunkCount = clamp(loadedChunkCount + 1, 0, chunkCount)
-            ui.get("loading_screen").get("asset_counter")
-                .text(loadedChunkCount .. "/" .. chunkCount)
-        end)
-            .loop(5)
-            .run()
-
     box.animate()
         .opacity(1, 1, "EASE_IN_OUT_CUBIC")
         .play()
@@ -75,47 +111,10 @@ function render(chunkCount)
         .play()
     progressBar.animate()
         .opacity(1, 2, "EASE_IN_OUT_CUBIC")
-        .progress(1, 5, "EASE_IN_OUT_EXPO")
+--         .progress(1, 5, "EASE_IN_OUT_EXPO")
         .play()
     assetCounter.animate()
         .opacity(1, 2, "EASE_IN_OUT_CUBIC")
         .move(vec2.of(0, -4), 2, "EASE_IN_OUT_CUBIC")
         .play()
-
-    scheduler
-        .task(function(task)
-            local screen = ui.get("loading_screen")
-
-            local fadeOutTime = 3
-            local easing = "EASE_OUT_BACK"
-            screen.get("background").animate()
-                .opacity(0, fadeOutTime, easing)
-                .play()
-            screen.get("test_sprite").animate()
-                .opacity(0, fadeOutTime, easing)
-                .move(vec2.of(0, -50), fadeOutTime, easing)
-                .play()
-            screen.get("test_text").animate()
-                .scale(vec2.of(3, 3), fadeOutTime, easing)
-                .opacity(0, fadeOutTime, easing)
-                .play()
-            screen.get("test_bar").animate()
-                .opacity(0, fadeOutTime, easing)
-                .move(vec2.of(0, 5), fadeOutTime, easing)
-                .play()
-            screen.get("asset_counter").animate()
-                .opacity(0, fadeOutTime, easing)
-                .move(vec2.of(0, 10), fadeOutTime, easing)
-                .play()
-
-            scheduler
-                .task(function(task)
-                    ui.get("loading_screen").remove()
-                    screenUnloaded = true
-                end)
-                .delay(40)
-                .run()
-                end)
-        .delay(70)
-        .run()
 end

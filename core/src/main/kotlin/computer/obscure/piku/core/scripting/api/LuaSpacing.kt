6212409ba0c0c626b8da7e38d4fb50bcdbf9340f
zNotChill <znotchill@znci.dev>
2026-05-16 21:37:29 +0100

    refactor: alter LuaSpacing rules

--- core/src/main/kotlin/computer/obscure/piku/core/scripting/api/LuaSpacing.kt
+++ core/src/main/kotlin/computer/obscure/piku/core/scripting/api/LuaSpacing.kt
@@ -5,35 +5,36 @@ import computer.obscure.twine.annotations.TwineFunction
 import computer.obscure.twine.annotations.TwineProperty
 import computer.obscure.twine.TwineNative
 
-// This follows all the rules of CSS' padding order
-// https://developer.mozilla.org/en-US/docs/Web/CSS/Reference/Properties/padding
 class LuaSpacing : TwineNative("spacing") {
-    @TwineFunction
-    fun of(all: Double): LuaSpacingInstance {
-        return LuaSpacingInstance(
-            left = all,
-            top = all,
-            right = all,
-            bottom = all
-        )
-    }
 
     @TwineFunction
-    fun of(x: Double, y: Double): LuaSpacingInstance {
-        return LuaSpacingInstance(
-            left = y, right = y,
-            top = x, bottom = x
-        )
-    }
+    fun of(all: Double): LuaSpacingInstance =
+        LuaSpacingInstance(all, all, all, all)
+
+    fun all(v: Double) = of(v)
 
     @TwineFunction
-    fun of(x: Double, y: Double, z: Double): LuaSpacingInstance {
-        return LuaSpacingInstance(
-            top = x,
-            right = y, left = y,
-            bottom = z
+    fun of(vertical: Double, horizontal: Double): LuaSpacingInstance =
+        LuaSpacingInstance(
+            top = vertical,
+            bottom = vertical,
+            left = horizontal,
+            right = horizontal
         )
-    }
+
+    fun verticalHorizontal(v: Double, h: Double) = of(v, h)
+
+    @TwineFunction
+    fun of(top: Double, horizontal: Double, bottom: Double): LuaSpacingInstance =
+        LuaSpacingInstance(
+            top = top,
+            bottom = bottom,
+            left = horizontal,
+            right = horizontal
+        )
+
+    fun topHorizontalBottom(t: Double, h: Double, b: Double) =
+        of(t, h, b)
 
     @TwineFunction
     fun of(
@@ -41,62 +42,40 @@ class LuaSpacing : TwineNative("spacing") {
         right: Double,
         bottom: Double,
         left: Double
-    ): LuaSpacingInstance {
-        return LuaSpacingInstance(
-            left = left,
+    ): LuaSpacingInstance =
+        LuaSpacingInstance(
             top = top,
             right = right,
-            bottom = bottom
+            bottom = bottom,
+            left = left
         )
-    }
+
+    fun trbl(top: Double, right: Double, bottom: Double, left: Double) =
+        of(top, right, bottom, left)
 
     companion object {
-        fun fromSpacing(spacing: Spacing): LuaSpacingInstance {
-            return LuaSpacingInstance(
+        fun fromSpacing(spacing: Spacing): LuaSpacingInstance =
+            LuaSpacingInstance(
                 left = spacing.left,
                 top = spacing.top,
                 right = spacing.right,
                 bottom = spacing.bottom
             )
-        }
     }
 }
 
 class LuaSpacingInstance(
+    @TwineProperty
     var left: Double = 0.0,
+    @TwineProperty
     var top: Double = 0.0,
+    @TwineProperty
     var right: Double = 0.0,
+    @TwineProperty
     var bottom: Double = 0.0
 ) : TwineNative() {
-
-    @TwineProperty("left")
-    var leftValue: Double
-        get() = left
-        set(value) { left = value }
-
-    @TwineProperty("top")
-    var topValue: Double
-        get() = top
-        set(value) { top = value }
-
-    @TwineProperty("right")
-    var rightValue: Double
-        get() = right
-        set(value) { right = value }
-
-    @TwineProperty("bottom")
-    var bottomValue: Double
-        get() = bottom
-        set(value) { bottom = value }
-
-    fun toSpacing(): Spacing {
-        return Spacing(
-            left = left,
-            top = top,
-            right = right,
-            bottom = bottom
-        )
-    }
+    fun toSpacing(): Spacing =
+        Spacing(left = left, top = top, right = right, bottom = bottom)
 
     @TwineFunction("tostring")
     override fun toString(): String {
